505cb51e76dcb0f448ea4b8d1edc31bea15e504c
Aakash Ayachit <110335913+AakashAyachit@users.noreply.github.com>
2024-04-25 00:42:32 +0530

    Update ass1a-bfs.cpp

--- ass1a-bfs.cpp
+++ ass1a-bfs.cpp
@@ -2,88 +2,51 @@
 #include<stdlib.h>
 #include<queue>
 using namespace std;
-
-
 class node
 {
    public:
-    
     node *left, *right;
-    int data;
-
-};    
-
+    int data;};    
 class Breadthfs
-{
- 
+{ 
  public:
- 
  node *insert(node *, int);
- void bfs(node *);
- 
-};
-
-
+ void bfs(node *);};
 node *insert(node *root, int data)
-// inserts a node in tree
 {
-
     if(!root)
-    {
-   	 
+    {	 
    	 root=new node;
    	 root->left=NULL;
    	 root->right=NULL;
    	 root->data=data;
-   	 return root;
-    }
-
+   	 return root;}
     queue<node *> q;
     q.push(root);
-    
     while(!q.empty())
     {
-
    	 node *temp=q.front();
-   	 q.pop();
-    
+   	 q.pop();  
    	 if(temp->left==NULL)
-   	 {
-   		 
+   	 { 		 
    		 temp->left=new node;
    		 temp->left->left=NULL;
    		 temp->left->right=NULL;
    		 temp->left->data=data;    
-   		 return root;
-   	 }
+   		 return root;}
    	 else
    	 {
-
-   	 q.push(temp->left);
-
-   	 }
-
+   	 q.push(temp->left);}
    	 if(temp->right==NULL)
-   	 {
-   		 
+   	 {		 
    		 temp->right=new node;
    		 temp->right->left=NULL;
    		 temp->right->right=NULL;
    		 temp->right->data=data;    
-   		 return root;
-   	 }
+   		 return root;}
    	 else
    	 {
-
-   	 q.push(temp->right);
-
-   	 }
-
-    }
-    
-}
-
-
+   	 q.push(temp->right);}}}
 void bfs(node *head)
 {
 
@@ -104,41 +67,24 @@ void bfs(node *head)
    			 {
    			   currNode = q.front();
    			   q.pop();
-   			   cout<<"\t"<<currNode->data;
-   			   
-   			 }// prints parent node
+   			   cout<<"\t"<<currNode->data;}
    			 #pragma omp critical
    			 {
    			 if(currNode->left)// push parent's left node in queue
    				 q.push(currNode->left);
    			 if(currNode->right)
-   				 q.push(currNode->right);
-   			 }// push parent's right node in queue   	 
-
-   		 }
-   	 }
-
-}
-
+   				 q.push(currNode->right);}}}}
 int main(){
-
     node *root=NULL;
     int data;
-    char ans;
-    
+    char ans;   
     do
     {
    	 cout<<"\n enter data=>";
-   	 cin>>data;
-   	 
-   	 root=insert(root,data);
-    
+   	 cin>>data;   	 
+   	 root=insert(root,data);    
    	 cout<<"do you want insert one more node?";
-   	 cin>>ans;
-    
-    }while(ans=='y'||ans=='Y');
-    
-    bfs(root);
-    
-    return 0;
-}
+   	 cin>>ans;    
+    }while(ans=='y'||ans=='Y');    
+    bfs(root);    
+    return 0;}
